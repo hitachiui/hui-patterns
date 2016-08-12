a14edfffa073edba589365673687791c57e1f04f
mark.cullen <mark.cullen@hitachicapital.co.uk>
2016-08-12 20:54:52 +0100

    Gulp plugin

--- index.js
+++ index.js
@@ -1,58 +1,13 @@
-/**
- * Scapegoat
- * https://github.com/brentertz/scapegoat
- *
- * Copyright (c) 2014 Brent Ertz
- * Licensed under the MIT license.
- */
+var through = require('through2');    // npm install --save through2
 
-var chars = {
-  '&amp;': '&',
-  '&quot;': '"',
-  '&#39;': '\'',
-  '&lt;': '<',
-  '&gt;': '>'
-};
-
-/**
- * Escape special characters in the given string of html.
- *
- * @param  {String} html
- * @return {String}
- */
-module.exports = {
-  escape: function(html) {
-    if (!html) {
-      return '';
-    }
-
-    var values = Object.keys(chars).map(function(key) { return chars[key]; });
-    var re = new RegExp('(' + values.join('|') + ')', 'g');
-
-    return String(html).replace(re, function(match) {
-      for (var key in chars) {
-        if (chars.hasOwnProperty(key) && chars[key] === match) {
-          return key;
-        }
-      }
-    });
-  },
-
+module.exports = function() {
   /**
-   * Unescape special characters in the given string of html.
-   *
-   * @param  {String} html
-   * @return {String}
+   * @this {Transform}
    */
-  unescape: function(html) {
-    if (!html) {
-      return '';
-    }
+  var transform = function(file, encoding, callback) {
+    file.content += 'HELLO WORLD';
+    callback();
+  };
 
-    var re = new RegExp('(' + Object.keys(chars).join('|') + ')', 'g');
-
-    return String(html).replace(re, function(match) {
-      return chars[match];
-    });
-  }
+  return through.obj(transform);
 };
